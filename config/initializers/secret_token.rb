3ae6e31262021782aa5e7c139c4d3f0baf304c53
Adam Wible <adam.wible@gmail.com>
2011-11-14 01:30:10 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fieldday::Application.config.secret_token = '827d516125326a46131ae1204a9ac95af33b7ff169ba2dbd5fdcd4592cf88f07694ba43c1500812700384dc1a7cb18bb5261b83b45638a06ee51b41a7266c4db'
