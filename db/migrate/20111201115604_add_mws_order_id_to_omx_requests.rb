08d55593b642b97ec9f7275ecd26731db96d6313
Adam Wible <adam.wible@gmail.com>
2011-12-04 11:47:02 +0100

    migration error

--- db/migrate/20111201115604_add_mws_order_id_to_omx_requests.rb
+++ db/migrate/20111201115604_add_mws_order_id_to_omx_requests.rb
@@ -3,7 +3,6 @@ class AddMwsOrderIdToOmxRequests < ActiveRecord::Migration
     add_column :omx_requests, :mws_order_id, :integer
     add_column :omx_requests, :keycode, :string
     add_column :omx_requests, :queue_flag, :boolean
-    add_column :omx_requests, :verify_flag, :boolean    
-    remove_column :omx_requests, :omx_request_id
+    add_column :omx_requests, :verify_flag, :boolean
   end
 end
