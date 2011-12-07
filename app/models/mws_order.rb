fcd81aa7faac02b6dd08b06669964a12df4994b4
Adam Wible <adam.wible@gmail.com>
2011-12-07 21:59:35 +0100

    Refresh RubyOmx

--- app/models/mws_order.rb
+++ app/models/mws_order.rb
@@ -55,11 +55,17 @@ class MwsOrder < ActiveRecord::Base
 	# Process XML order into ActiveRecord, and process items on order
 	def process_order(mws_connection)		
 		return_code = fetch_order_items(mws_connection)
-		#raise Exception unless (self.item_quantity == (self.number_of_items_unshipped + self.number_of_items_shipped))
+		
+		# retry one time if problem
+		if (self.item_quantity != (self.number_of_items_unshipped + self.number_of_items_shipped))
+			return_code = fetch_order_items(mws_connection)
+		end
+		
 		#TODO if reprocessing, use the update API call rather than append
 		if self.fulfillment_channel == "MFN"
 			append_to_omx
 		end
+		
 		return return_code
 	end
 	
