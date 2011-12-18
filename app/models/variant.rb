f94e549acfede672314fbe87458945e76823ac55
Adam Wible <adam.wible@gmail.com>
2011-12-18 18:35:25 +0100

    foreign key again

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -2,7 +2,7 @@ class Variant < ActiveRecord::Base
 	belongs_to :product
 	#delegate_belongs_to :product, :name, :description, :available_on, :meta_description, :meta_keywords
 	has_many :variant_images, :dependent => :destroy
-	has_many :mws_order_items, :primary_key => "sku", :foreign_key => "sku", 
+	has_many :mws_order_items, :primary_key => "sku", :foreign_key => "clean_sku"
 	
 	validates_uniqueness_of :sku
 end
