ac99a11a5c3ae9f7e8b15bba03233a3c3fdd9c56
AE Wible <aewible@gmail.com>
2012-04-12 04:59:55 +0200

    re-sync migrations and production db

--- db/migrate/20120412023109_add_authenticated_url_to_stores.rb
+++ db/migrate/20120412023109_add_authenticated_url_to_stores.rb
@@ -1,6 +1,5 @@
 class AddAuthenticatedUrlToStores < ActiveRecord::Migration
   def change
-    #remove_column :stores, :authenticated_url, :string
-    #add_column :stores, :authenticated_url, :string
+    add_column :stores, :authenticated_url, :string
   end
 end
